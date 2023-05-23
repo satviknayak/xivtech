d941be747dab318400f5aeb92577f0d5260c92c6
Satvik S Nayak <nayaksatvik02@gmail.com>
2023-05-23 22:25:09 +0530

    update urls

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,28 @@ const ServicesContent = [
     title:'AI + RPA is what we do',
     image:'rp2.jpg',
     paragraph:'Future-Proof your business. Drive efficiency, profitability and deliver on customer experience',
-    url:'', 
+    url:'https://www.xivtech.io./services/AIandRPAautomation', 
     link:'AI + RPA Automation'
   },
   {
     title:'Make Bolder Choices',
     image:'p1.png',
     paragraph:'Digital focused strategies to realize market-changing ideas',
-    url:'', 
+    url:'https://www.xivtech.io./services/enterprise-apps', 
     link:'Build Better Apps'
   },
   {
     title:'Innovate with Speed',
     image:'p2.jpg',
     paragraph:'Create higher quality software, deliver on customer expectations and business goals',
-    url:'', 
+    url:'https://www.xivtech.io./services/delivery-pipeline-automation', 
     link:'DevOps'
   },
   {
     title:'Embrace Cloud',
     image:'p3.jpg',
     paragraph:'With Cloud-First accelerate innovation and optimize performance',
-    url:'', 
+    url:'https://www.xivtech.io./services/cloud', 
     link:'Cloud Services'
   }
 ]
